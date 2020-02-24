33573f9e81d847e47f0901d3ababb4f2e45a9b4b
vitor-apolinario <vitorapoli@gmail.com>
2020-02-24 12:10:48 -0300

    Initial commit

---
+++ src/main/java/listavip/ConvidadoController.java
@@ -0,0 +1,13 @@
+package listavip;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ConvidadoController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
